bf15eba5a4e165e14a8899534e4521e1a68174e1
0sol0 <jdsmy1004@gmail.com>
2022-10-27 12:13:05 +0900

    Day4 & Day5

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from articles import views
 
+app_name = 'articles'
 urlpatterns = [
     path('index/', views.index),
-    path('dinner/<str:name>/', views.dinner),
+    path('dinner/', views.dinner, name="dinner"),
     path('review/', views.review, name="review"),
-    path('create-review/', views.create_review, name="create_review"),
+    path('create_review/', views.create_review, name="create_review"),
+    path('<int:pk>/', views.detail, name="detail"),
+    path('<int:pk>/delete/', views.delete, name="delete"),
+    path('<int:pk>/edit/', views.edit, name="edit"),
+    path('<int:pk>/update/', views.update, name="update"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
+from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def dinner(request, name):
+def dinner(request):
     menus = [{'name':'족발','price':30000}, {'name':'햄버거','price':5000}, {'name':'치킨','price':20000}, {'name':'초밥','price':15000}]
     pick = random.choice(menus)
+    articles = Article.objects.order_by('-create_at')
     context = {
         'pick':pick,
-        'name':name,
         'menus':menus,
+        'articles':articles
     }
     
     return render(request, 'dinner.html', context)
@@ -20,8 +22,40 @@ def review(request):
     return render(request, 'review.html')
 
 def create_review(request):
+    title =  request.POST.get('title')
     content =  request.POST.get('content')
+    article = Article(title=title, content=content)
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
     context = {
-        'content':content,
+        'article':article
     }
-    return render(request, 'review_result.html', context)
+
+    return render(request, 'detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        article.delete()
+    
+    return redirect('articles:dinner')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    
+    return redirect('articles:detail', article.pk)
